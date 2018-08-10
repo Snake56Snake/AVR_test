5c67799a791194384a97f8c8bd15774c7a95aa88
Jan Hoof <Jan.Hoof@cleanlaser.de>
2018-08-10 21:50:47 +0200

    I2C Temperatur funktioniert

--- main.c
+++ main.c
@@ -18,8 +18,10 @@
 char c_buffer[10];
 int8_t i_test;
 int8_t i_temperatur;
+int8_t i_temperatur_comma;
 float f_temperatur;
-int8_t i_buffer_I2C;
+int8_t i_buffer_I2C_0;
+int8_t i_buffer_I2C_1;
 int8_t i_buffer_I2C_2;
 
 
@@ -34,50 +36,23 @@ int main(void)
     while (1)
     {
 
-//    	dtostrf();
-
-		lcd_setcursor(0,1);
-		lcd_string("Zeile 1____________1");
-
-		lcd_setcursor(0,2);
-		lcd_string("Zeile 2____________2");
-
-		lcd_setcursor(0,3);
-		lcd_string("Zeile 3____________3");
-
-		lcd_setcursor(0,4);
-		lcd_string("Zeile 4____________4");
-
-
     	PORTD |= (1<<PD5); // LED1 On
-		_delay_ms(50);
-
-
-
-		PORTD &= ~(1<<PD5); 	// LED1 Off
-	 	_delay_ms(50);
-    /*
-    	_delay_ms(50);
-
-    	PORTD |= (1<<PD5); // LED1 On
-		_delay_ms(50);
+    	lcd_clear();
 
 		i_test ++;
-
 		itoa( i_test, c_buffer, 10);			// display  test Counter
-		lcd_setcursor(0,3);
+		lcd_setcursor(12,1);
 		lcd_string(c_buffer);
 
-		lcd_setcursor(0,1);
-		lcd_string("Schritt 1");
+
+//
+//	 	_delay_ms(100);
+//
+//
+//
+//		_delay_ms(50);
 
 
-		lcd_setcursor(0,1);
-		lcd_string("Schritt 2");
-
-
-		lcd_setcursor(0,1);
-		lcd_string("Schritt 3");
 
 
 		ret = i2c_start(LM75_ADDRESS+I2C_READ);
@@ -89,57 +64,51 @@ int main(void)
 			lcd_setcursor(0,1);
 			lcd_string("Fehler I2C");
 
+//			lcd_setcursor(0,4);
+//			lcd_string("               ");
 
 		}
 		else{
 
+			i_buffer_I2C_0 = i2c_read(1);
+			i_buffer_I2C_1 = i2c_read(0);
+			i2c_stop();
+
 			lcd_setcursor(0,1);
 			lcd_string("Erfolg I2C");
 
-			i_buffer_I2C_2 = i2c_readNak();
-			i_buffer_I2C = i2c_readNak();
-			i2c_stop();
+			i_temperatur = i_buffer_I2C_0;
 
-			i_temperatur = i_buffer_I2C * 0.5;      // LM75 stores temp in 0.5 °
+			if (i_buffer_I2C_1 & (1<<8)){
+				i_temperatur_comma = 5;
+			}
+			else{
+				i_temperatur_comma = 0;
+			}
 
-//			sprintf();
-
-//			ftoa(f_temperatur, c_buffer, 10);
-
-			itoa( i_temperatur, c_buffer, 10);		// display  test Counter
-			lcd_setcursor(0,4);
-			lcd_string(c_buffer);
 
 		}
 
+		itoa( i_temperatur, c_buffer, 10);
+		if(i_temperatur_comma==5){
+			strcat(c_buffer,",5");
+		}
+		else{
+			strcat(c_buffer,",0");
+		}
 
 
+		lcd_setcursor(0,4);
+		lcd_string(c_buffer);
 
 
-//		 i2c_start_wait(LM75_ADDRESS+I2C_READ); // device address and write mode
-//		 i2c_read(i_buffer_I2C);
-//
-//		 i2c_start_wait(LM75_ADDRESS+I2C_WRITE); // device address and write mode
-//		 i2c_write(0x00);                        // write address = 0 (temperature)
-//		 i2c_rep_start(i_buffer_I2C+I2C_READ);   // set device address and read mode
-//		 i_buffer_I2C = i2c_readNak();           // read one byte from LM75
-//		 i2c_stop();
-
-
-//		i_temperatur = i_buffer_I2C * 0.5;       // LM75 stores temp in 0.5 °
-//
-//		itoa( i_temperatur, c_buffer, 10); 		// display temperatur
-//		lcd_setcursor(0,2);
-//		lcd_string(c_buffer);
-
-*/
-
-
-
-		PORTD &= ~(1<<PD5); 	// LED1 Off
-
+	    PORTD &= ~(1<<PD5); 	// LED1 Off
+	    _delay_ms(100);
+	    _delay_ms(100);
+	    _delay_ms(100);
+	    _delay_ms(100);
+	    _delay_ms(100);
     }
-
     return (1);	// should never happen
 }
 
